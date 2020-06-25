424c3c8d796fcbdf63798b1db53adaef1405fb1d
Osehi <osehiaseehilen@gmail.com>
2020-06-25 13:31:54 +0100

    icon and title

--- lib/main.dart
+++ lib/main.dart
@@ -21,17 +21,61 @@ class TODOApp extends StatefulWidget {
 class _TODOAppState extends State<TODOApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: ListView(
-          children: <Widget>[
-            Icon(
-              Icons.filter_vintage,
-              size:50,
-              color:Colors.black
+    
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(30.0),
+        alignment:Alignment.center,
+        color:Colors.white,
+        child: Column(
+         
+          children:<Widget>[
+            
+            Row(
+              
+              mainAxisAlignment: MainAxisAlignment.start,
+              
+              children:[
+              
+                Icon(
+                Icons.filter_vintage,
+                size: 50,
+                color: Colors.black,
+              )
+              ]
+              
+            ),
+          
+            Row(
+              
+              children:<Widget>[
+                
+                Expanded(child: Text('Task',
+              textDirection:TextDirection.ltr,
+              textAlign: TextAlign.right,
+              style: TextStyle(
+                
+                decoration:TextDecoration.none,
+                fontSize:24.0,
+                color:Colors.black
+                
+                
+              ),
+              ),),
+              
+              Expanded(child: Text('List',
+              textDirection:TextDirection.ltr,
+              style:TextStyle(
+                decoration: TextDecoration.none,
+                fontSize: 20.0,
+                color: Colors.grey
+                )
+              ))
+              ]
+              
             )
           ],
-          )
+        ),
       ),
     );
   }
